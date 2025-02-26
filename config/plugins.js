02072a85b3221dae7a2add1619914534969a2ab4
Surya10s <kandansuryamass@gmail.com>
2025-02-26 12:26:23 +0530

    cloudinary issue

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,16 @@
 module.exports = ({ env }) => ({
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: env('CLOUDINARY_CLOUD_NAME'),
-      api_key: env('CLOUDINARY_API_KEY'),
-      api_secret: env('CLOUDINARY_API_SECRET'),
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+            cloud_name: 'dvosfhyof',
+            api_key: '223883866316725',
+            api_secret: 'sBODpn6IiLd-qp0uEY8wLTou7m0',
+          },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
     },
-  },
-});
-
+  });
