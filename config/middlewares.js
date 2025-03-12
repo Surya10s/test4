fe2e89b2adc0be09e2332965e197597291fa0fe3
Surya10s <kandansuryamass@gmail.com>
2025-03-12 17:08:47 +0530

    solved the cloudinary image proplem

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::logger',
-  'strapi::errors',
+  'strapi::errors',{
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
